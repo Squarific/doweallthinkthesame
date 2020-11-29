d162c6c34f689daae5d3b60724254aa58de746d9
Filip Smets <filipsmetsbelgium@gmail.com>
2020-11-29 12:16:34 +0100

    First version of server

--- server/Room.js
+++ server/Room.js
@@ -1,3 +1,35 @@
+class Room {
+    constructor() {
+        this.sockets = [];
+    }
 
+    /**
+     * Join the socket to this room
+     */
+    join (socket) {
+        this.sockets.push(socket);
+    }
 
-module.exports = Room;
+    /**
+     * Leave the socket from this room
+     */
+    leave (socket) {
+        let index;
+        while (index = this.sockets.indexOf(socket) > -1) {
+            this.sockets.splice(index, 1);
+        }
+    }
+
+    ensureActiveClients () {
+        this.sockets = this.sockets.filter((socket) => socket.readyState === WebSocket.OPEN);
+    }
+
+    send (message) {
+        this.ensureActiveClients();
+        for (let index = 0; index < this.sockets.length; index++) {
+            this.sockets[index].send(message);
+        }
+    }
+}
+
+export default Room;
--- server/main.js
+++ server/main.js
@@ -1,16 +1,45 @@
 const WebSocket = require('ws');
-const Room = require('./Room.js');
+const Room = require('./Room.js').default;
+const rooms = {};
+
+const MIN_TIME_BETWEEN_MESSAGES_OF_ONE_CLIENT = 40;
  
 function heartbeat() {
   this.isAlive = true;
 }
+
+function joinRoom (socket, room) {
+  if (typeof room !== 'string' || room.length < 5 || room.length > 10) socket.terminate();
+  if (!rooms[room]) rooms[room] = new Room();
+
+  rooms[room].join(socket);
+  socket.room = rooms[room];
+}
+
+function cleanEmptyRooms () {
+  // For every room ensureActiveClients
+  // if clients < 1, remove
+  // TODO
+  // TODO also call this every so often
+}
+
+function handleMessage (message) {
+  if (Date.now() - this.lastSent < MIN_TIME_BETWEEN_MESSAGES_OF_ONE_CLIENT) return;
+  if (!this.room) return joinRoom(this, message);
+  this.room.send(message);
+  this.lastSent = Date.now();
+}
  
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({
+  port: 8080,
+  maxPayload: 1024
+});
  
 wss.on('connection', function connection(ws) {
   ws.isAlive = true;
   ws.on('pong', heartbeat);
-  
+  ws.on('message', handleMessage);
+  ws.lastSent = 0;
 });
  
 const interval = setInterval(function ping() {
